8b783ad1ec68a1b10644dfdbc0522d5ce34b81d3
Mekan777-alt <mekan2678@gmail.com>
2023-07-29 23:06:11 +0300

    add context and index.html

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
